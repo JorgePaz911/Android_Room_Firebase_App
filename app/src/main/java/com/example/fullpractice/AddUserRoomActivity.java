51230bd837b0550ea96ced399b375dc235785b01
JorgePaz <jorge.pm180@gmail.com>
2021-05-29 23:55:57 -0700

    Before developing view model

--- app/src/main/java/com/example/fullpractice/AddUserRoomActivity.java
+++ app/src/main/java/com/example/fullpractice/AddUserRoomActivity.java
@@ -2,13 +2,44 @@ package com.example.fullpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.fullpractice.model.User;
+import com.example.fullpractice.util.UserRoomDatabase;
 
 public class AddUserRoomActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText email;
+    private EditText age;
+    private Button submitBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user_room);
+
+        name = findViewById(R.id.name_et);
+        email = findViewById(R.id.email_et);
+        age = findViewById(R.id.age_et);
+        submitBtn = findViewById(R.id.submit_btn);
+
+        submitBtn.setOnClickListener(v -> {
+            User user = new User(name.getText().toString(), email.getText().toString(), Integer.parseInt(age.getText().toString()));
+            saveNewUser(user);
+        });
+
+    }
+
+    private void saveNewUser(User user) {
+        UserRoomDatabase db = UserRoomDatabase.getDatabase(this.getApplicationContext());
+        db.userDao().insert(user);
+        startActivity(new Intent(AddUserRoomActivity.this, MainActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/fullpractice/model/User.java
+++ app/src/main/java/com/example/fullpractice/model/User.java
@@ -30,6 +30,9 @@ public class User {
         this.age = age;
     }
 
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
     public int getUserId() {
         return userId;
     }
